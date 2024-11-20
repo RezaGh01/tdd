ebc86d722110f2cf36b4d662917ff02a5962b320
Farid Keramat <farid.keramat@tapsi.cab>
2024-11-20 10:24:50 +0330

    Makes money variables final

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class Money {
-    protected int amount;
-    protected String currency;
+    protected final int amount;
+    protected final String currency;
 
     public Money(int amount, String currency) {
         this.amount = amount;
